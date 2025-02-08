e0f68064d8d2f82f8894a8ecdce1bbb562174b2a
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2025-02-08 19:22:19 +0100

    retrieving initial messages, added middleware for better session management

--- src/app/[...url]/page.tsx
+++ src/app/[...url]/page.tsx
@@ -1,6 +1,7 @@
 import ChatWrapper from "@/components/ChatWrapper";
 import { ragChat } from "@/lib/rag-chat";
 import { redis } from "@/lib/redis";
+import { cookies } from "next/headers";
 import React from "react";
 
 interface IPageProps {
@@ -12,6 +13,7 @@ const urlFixer = (url: string[]) => {
 };
 
 const Page = async (props: { params: Promise<IPageProps> }) => {
+  const sessionCookie = (await cookies()).get("sessionId")?.value;
   const params = await props.params;
   console.log("params", params);
 
@@ -19,11 +21,19 @@ const Page = async (props: { params: Promise<IPageProps> }) => {
 
   //reconstructing url
   const fixedUrl = urlFixer(params.url as string[]);
+  //creating session id by merging url and session cookie
+  // and replacing all the slashes so they don't cause issues
+  const sessionId = (fixedUrl + "@@" + sessionCookie).replace(/\//g, "");
 
   //check if url is already indexed so we don't index it again
   const isUrlAlreadyIndexed = await redis.sismember("indexed-urls", fixedUrl);
-  console.log("isUrlAlreadyIndexed", isUrlAlreadyIndexed);
 
+  //retrieving the last 10 messages
+  //TODO: implement infinite scrolling
+  const initialMessages = await ragChat.history.getMessages({
+    amount: 10,
+    sessionId,
+  });
   //if not indexed, index it and add to indexed-urls
   if (!isUrlAlreadyIndexed) {
     await ragChat.context.add({
@@ -35,9 +45,9 @@ const Page = async (props: { params: Promise<IPageProps> }) => {
     await redis.sadd("indexed-urls", fixedUrl);
   }
 
-  const sessionId = "mock-session";
-
-  return <ChatWrapper sessionId={sessionId} />;
+  return (
+    <ChatWrapper sessionId={sessionId} initialMessages={initialMessages} />
+  );
 };
 
 export default Page;
