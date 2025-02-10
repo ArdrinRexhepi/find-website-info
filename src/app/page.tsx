de26f115db401227150c5d6de9cf8703a2e22bca
ArdrinRexhepi <ardrin1rexhepi@gmail.com>
2025-02-10 14:14:20 +0100

    fixed sizing issue for phones on landing page and improved text sizing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <BackgroundBeamsWithCollision className="bg-zinc-900 items-center flex flex-col h-full">
       <div className="flex flex-col gap-2">
-        <h2 className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-white font-sans tracking-tight max-w-4xl">
+        <h2 className="relative z-20 text-4xl min-[405px]:text-5xl sm:text-7xl font-bold text-center text-white font-sans tracking-tight max-w-4xl">
           Welcome to
           <div className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
             <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
@@ -17,17 +17,14 @@ export default function Home() {
             </div>
           </div>
         </h2>
-        <div className="flex flex-col">
+        <div className="flex flex-col text-sm md:text-lg text-center text-gray-200/75">
           <TextAnimate
             animation="blurInUp"
             by="character"
-            className="text-lg text-center text-gray-200">
+            className="inline-block">
             Do you want to find more information about a website?
           </TextAnimate>
-          <TextAnimate
-            animation="blurInUp"
-            by="character"
-            className="text-lg text-center mb-6 text-gray-200">
+          <TextAnimate animation="blurInUp" by="character" className="mb-4">
             No problem! Enter a website URL to analyze it using
             Retrieval-Augmented Generation (RAG).
           </TextAnimate>
